0ba8fa0604bf1b88484055fb1f41e6d912674598
ultrasnap <ultrasnap@yahoo.com>
2015-02-18 09:35:06 -0500

    Update game.js

--- game_files/RPG/game.js
+++ game_files/RPG/game.js
@@ -1,3 +1,11 @@
+var playersprite = new Image();
+playersprite.src = 'grass.png'
+var player = {
+	currentSprite: playersprite,
+	spriteWidth: 16,
+}
+var tick = 0;
+var frame = 0;
 
 function loadGame(){
 	game_init()
@@ -16,7 +24,21 @@ function drawTiles(){
 }
 
 function drawPlayer(){
-	
+	ctx.drawImage(player.currentSprite,player.x,player.y)
+}
+
+function animatePlayer(){
+	if (player.walking){
+		if(player.movingRight){
+			if(frame % 2 == 0){
+				player.currentSpriteX = 1*player.spriteWidth
+			}
+			else{
+				player.currentSpriteX = 2*player.spriteWidth
+			}
+		}
+	}
+	else{player.currentSpriteX = 0}
 }
 
 function game_init(){
@@ -25,9 +47,13 @@ function game_init(){
 }
 
 function gameLoop(){
+	tick += 1
+	if(tick % 16 == 0){frame ++}
 	ctx.clearRect(0,0,480,480)
 	ctx.fillStyle='#c79a33'
 	ctx.fillRect(0,0,480,480)
 	drawTiles()
+	ctx.font = '20px Georgia'
+	ctx.fillText(frame,100,100)
 	requestAnimationFrame(gameLoop)
 }
