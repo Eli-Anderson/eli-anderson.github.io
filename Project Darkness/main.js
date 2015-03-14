d911d55368734d7b74d539117ff74ecfbc1592e5
ultrasnap <ultrasnap@yahoo.com>
2015-03-13 22:25:29 -0400

    Update main.js

--- Project Darkness/main.js
+++ Project Darkness/main.js
@@ -16,7 +16,7 @@ var map1 =  []
 var objects1 = []
 var source;
 function init(){
-	setTimeout(getFile,500)
+	setTimeout(reload,500)
 	setInterval(getWidth,600)
 }
 function getWidth(){
@@ -24,7 +24,6 @@ function getWidth(){
 	    width = Number(document.getElementById('t_width').value);
     }
     else{width = 32}
-    //drawSidebar();
 }
 function loop(){
 	map_width = Number(document.getElementById('m_width').value);
@@ -33,7 +32,19 @@ function loop(){
 	setTimeout(loop,100)
 }
 
-
+function getImg(e){
+    var file = e.target.files[0];
+    if(file){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            var source = e.target.result;
+            tiles.src = source
+            reload()
+        }
+        reader.readAsDataURL(file);
+    }
+}
+document.getElementById('t_upload').addEventListener('change',getImg)
 function drawSidebar(){
 	side.clearRect(0,0,256,450)
     if(Number(document.getElementById('t_width').value) > 0){
@@ -61,9 +72,13 @@ function drawSidebar(){
     else{side.drawImage(tiles,selected*width,0,width,width,224,418,32,32)}
 }
 
-function getFile(){
+function reload(){
+        main.clearRect(-5000,-5000,10000,10000)
+        objectlayer.clearRect(-5000,-5000,10000,10000)
+        side.clearRect(0,0,256,450)
 	    drawSidebar()
 	    overlayGrid()
+	    drawSelectionBorder()
 	    getAttr()
 }
 
@@ -156,12 +171,9 @@ function getMap(e){
             eval(text)
             map1 =  map;
             objects1 = objects;
-            //objects1 = text.slice(text.lastIndexOf('='),text.length)
-            //map1 =  eval(map)
-            //objects1 = eval(objects1)
             document.getElementById('m_width').value = map1[0].length;
             document.getElementById('m_height').value = map1.length;
-            getFile()
+            reload()
         }
         reader.readAsText(file);
     }
@@ -174,68 +186,189 @@ function getAttr(){
     document.getElementById('attr1_val').value = lr;
     document.getElementById('attr2_val').value = lb;
 }
+var selection_counter = 0;
+var selection_point1 = 0;
+var selection_point2 = 0;
+
+function getSelection(){
+    var x = mouse.x
+	var y = mouse.y
+	var x2 = Math.abs(x % Math.round(width*total_scale));
+	var y2 = Math.abs(y % Math.round(width*total_scale));
+	var x3 = x-x2;
+	var y3 = y-y2;
+	var w = Math.round(width*total_scale)
+	x3 = Math.round(x3-x5);
+	y3 = Math.round(y3-y5);
+    if(selection_counter%2 == 0){
+        selection_point1 = {
+            x: Math.round(x3/w),
+            y: Math.round(y3/w),
+        }
+    }
+    else{
+        selection_point2 = {
+            x: Math.round(x3/w),
+            y: Math.round(y3/w),
+        }
+    }
+    drawSelectionBorder()
+    selection_counter++
+}
+function drawSelectionBorder(){
+    var w = selection_point2.x - selection_point1.x;
+    var h = selection_point2.y - selection_point1.y;
+    var w2 = Math.round(width*total_scale)
+    objectlayer.strokeStyle='yellow';
+    objectlayer.lineWidth='3'
+    objectlayer.beginPath()
+    objectlayer.moveTo((selection_point1.x+1*w2),(selection_point1.y*w2));
+    objectlayer.lineTo((selection_point1.x*w2),(selection_point1.y*w2));
+    objectlayer.lineTo((selection_point1.x*w2),(selection_point1.y+1*w2));
+    objectlayer.stroke()
+    objectlayer.beginPath()
+    objectlayer.rect((selection_point1.x*w2),(selection_point1.y*w2),(w*w2),(h*w2))
+    objectlayer.closePath()
+    objectlayer.stroke()
+}
+function fillSelection(){
+    //set map
+    var w = Math.abs(selection_point2.x - selection_point1.x);
+    var h = Math.abs(selection_point2.y - selection_point1.y);
+    var sy = (selection_point1.y)
+    var sx = (selection_point1.x)
+    for (var y=0; y<h; y++){
+        for (var x=0; x<w; x++){
+            map1[y+sy][x+sx] = selected;
+        }
+    }
+    reload()
+    
+}
 
 
+document.addEventListener('keydown',handleKey)
+function handleKey(e){
+    if(e.keyCode==32){
+        getSelection()
+    }
+    if(e.keyCode==189){
+        handleZoom('out')
+    }
+    else if(e.keyCode==187){
+        handleZoom('in')
+    }
+    if(e.keyCode==86){
+        log(total_scale+',')
+    }
+    if(e.keyCode==27){
+	    selection_point1.x = -1;
+        selection_point1.y = -1;
+        selection_point2.x = -1;
+        selection_point2.y = -1;
+        selection_counter = 0;
+    }
+    if(e.keyCode==37){
+        //left
+        x5 -= (width*total_scale);
+        main.translate(-width*total_scale,0);
+        reload();
+    }
+    else if(e.keyCode==39){
+        //right
+        x5 += (width*total_scale);
+        main.translate(width*total_scale,0);
+        reload();
+    }
+    if(e.keyCode==38){
+        //up
+        y5 -= (width*total_scale);
+        main.translate(0,-width*total_scale);
+        reload();
+    }
+    else if(e.keyCode==40){
+        //down
+        y5 += (width*total_scale);
+        main.translate(0,width*total_scale);
+        reload();
+    }
+    //log(e.keyCode)
+}
 
 x5 = 0;
 y5 = 0;
 var start = false;
 function handleClickMain(){
+    getStartCoords()
     var lr = document.getElementById('attr1_val').value;
     var lb = document.getElementById('attr2_val').value;
     var lf = document.getElementById('attr3').checked;
     
 	var x = mouse.x
 	var y = mouse.y
-	var x2 = Math.abs(x % width);
-	var y2 = Math.abs(y % width);
+	var x2 = Math.abs(x % Math.round(width*total_scale));
+	var y2 = Math.abs(y % Math.round(width*total_scale));
 	var x3 = x-x2;
 	var y3 = y-y2;
+	var w = Math.round(width*total_scale)
+	x3 = Math.round(x3-x5);
+	y3 = Math.round(y3-y5);
 	if(mouse.down){
     	if(selected != 'drag'){
-    		if(map1[(y3)/width] !== undefined){
-    	 	   if(map1[(y3)/width][(x3)/width] !== undefined){
+    		if(map1[(y3)/w] !== undefined){
+    	 	   if(map1[(y3)/w][(x3)/w] !== undefined){
     	        	if(selected === 0){
     	        	    main.clearRect(x3-x5,y3-y5,width,width)
-    	        	    map1[(y3)/width][(x3)/width] = selected;
+    	        	    objectlayer.clearRect(x3-x5,y3-y5,width,width)
+    	        	    map1[(y3)/w][(x3)/w] = selected;
     	        	}
     	        	else if(selected > 90){
     	        	    for(var i=0; i<objects1.length; i++){
-    	        	        if(objects1[i][4]==x3/width && objects1[i][5]==y3/width){
+    	        	        if(objects1[i][4]==x3/w && objects1[i][5]==y3/w){
     	        	            objects1.splice(i,1)
     	        	        }
     	        	    }
-    	        	    objects1.push([selected,lr,lb,lf,x3/width,y3/width])
+    	        	    objects1.push([selected,lr,lb,lf,x3/w,y3/w])
     	        	    objectlayer.drawImage(light,x3,y3,width,width)
     	        	}
     	        	else{
-    	        	    map1[(y3)/width][(x3)/width] = selected;
-    	        	    main.drawImage(tiles,selected*width,0,width,width,x3,y3,width,width);
+    	        	    map1[(y3)/w][(x3)/w] = selected;
     	        	}
     	    	}
     		}
     	}
+    	reload()
     }
-	if(mouse.down && start === false){start_coords = [x3,y3];start = true;}
+}
+function getStartCoords(){
+	if(mouse.down && start == false){
+	    var x = mouse.x
+		var y = mouse.y
+		var x2 = Math.abs(x % Math.round(width*total_scale));
+	    var y2 = Math.abs(y % Math.round(width*total_scale));
+		var x3 = x-x2;
+		var y3 = y-y2;
+	    start_coords = [x3,y3];
+	    start = true;
+	}
 }
 function handleDrag(){
 	if(mouse.down){
 		var x = mouse.x
 		var y = mouse.y
-		var x2 = x % width;
-		var y2 = y % width;
+		var x2 = x % (width*total_scale);
+		var y2 = y % (width*total_scale);
 		var x3 = x-x2;
 		var y3 = y-y2;
 		var x4 = x3 - start_coords[0];
 		var y4 = y3 - start_coords[1];
-
 		if(selected == 'drag'){
 		    x5 += x4;
 		    y5 += y4;
-		    if(Math.abs(x4) > width/2 || Math.abs(y4) > width/2){
+		    if(Math.abs(x4) > (width*total_scale)/2 || Math.abs(y4) > (width*total_scale)/2){
 		    	main.translate(x4,y4)
-		    	main.clearRect(-5000,-5000,10000,10000)
-		    	overlayGrid()
+		    	objectlayer.translate(x4,y4)
+		    	reload()
 		    }
 		}
 	}
@@ -254,13 +387,32 @@ function handleClickSide(e){
 		    if(x > i*width && x <(i+1)*width && y > n*width && y < width+(n*width)){
 		        if(i+ (n*8) < tiles.width/width){
 		    	    selected = i +(n*8);
+		    	    if(selection_point2.x-selection_point1.x != 0 &&
+		    	    selection_point2.y-selection_point1.y != 0){
+		    	        fillSelection()
+		    	        
+		    	    }
 		        }
 		    }
 	    }
 	}
 	drawSidebar()
 }
-
+var total_scale = 1;
+function handleZoom(dir){
+    var scaler;
+    if(dir == 'out'){
+        scaler = (3/4)
+        main.scale(scaler,scaler)
+        total_scale = total_scale*scaler
+    }
+    else{
+        scaler = (4/3);
+        main.scale(scaler,scaler)
+        total_scale = total_scale*scaler
+    }
+    reload()
+}
 document.getElementById('main').addEventListener('mousedown',function(){mouse.down = true;handleClickMain()})
 document.getElementById('main').addEventListener('mousemove',handleMove)
 document.getElementById('main').addEventListener('mouseup',handleClickUpMain)
@@ -270,13 +422,28 @@ canvas2.addEventListener('click',handleClickSide)
 var mouse = {
 	x: 0,
 	y: 0,
+	clientx: 0,
+	clienty: 0,
 	down: false,
 }
+var rect = canvas.getBoundingClientRect()
 function handleMove(e){
-	mouse.x = e.clientX - 10 - x5;
-	mouse.y = e.clientY - 10 - y5;
-	handleClickMain()
-	handleDrag()
+    mouse.clientx = e.clientX - rect.left;
+    mouse.clienty = e.clientY - rect.top;
+	mouse.x = mouse.clientx - x5;
+	mouse.y = mouse.clienty - y5;
+	handleClickMain();
+	handleDrag();
+	reload();
+	objectlayer.strokeStyle='black';
+	objectlayer.lineWidth=1;
+	objectlayer.beginPath();
+	objectlayer.moveTo(mouse.clientx-5,mouse.clienty);
+	objectlayer.lineTo(mouse.clientx+5,mouse.clienty);
+	objectlayer.moveTo(mouse.clientx,mouse.clienty-5);
+	objectlayer.lineTo(mouse.clientx,mouse.clienty+5);
+	objectlayer.stroke()
+	
 }
 
 function handleClickUpMain(){
@@ -284,6 +451,9 @@ function handleClickUpMain(){
 	start = false;
 }
 window.onload = init();
+
+
+//document.getElementById('body').setAttribute('class',"stop-scrolling")
 
 function log(text){
     document.getElementById('log').innerHTML += text
