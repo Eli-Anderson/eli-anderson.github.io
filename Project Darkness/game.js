70d0f9b33f03002b89ff2f30e36f2aeed84e6ea6
ultrasnap <ultrasnap@yahoo.com>
2015-03-20 12:29:53 -0400

    Update game.js

--- Project Darkness/game.js
+++ Project Darkness/game.js
@@ -595,7 +595,7 @@ function castRays(x,y,r,so,eo){
     	}
     }
     for(var n=0; n<endpoints.length; n++){
-    	console.log(endpoints[n])
+    	//console.log(endpoints[n])
 	    ex = endpoints[n][0];
 	    ey = endpoints[n][1];
 	    
