2da3e22310197dc3277f9d7578dd3cd52f4df4d4
ultrasnap <ultrasnap@yahoo.com>
2015-04-16 16:14:44 -0400

    Updates

--- Runner/main.js
+++ Runner/main.js
@@ -10,9 +10,10 @@ function init(){
 var game = {
 	frame: 0,
 	running: false,
+	trigger1Fired: false,
 	
 	restart: function(){
-	    player.y = 10;
+	    player.y = 155-player.h/2;
 	    player.dy = 0;
 	    player.ddy = 0;
 	    player.totalPoints += player.points;
@@ -20,6 +21,8 @@ var game = {
 	    
 	    walls = [];
 	    coins = [];
+
+	    coin_vars.y_scaler = 110;
 	    
 	    game.awaitingInput = true;
 	    game.running = true;
@@ -27,8 +30,8 @@ var game = {
 };
 function game_loop(){
 	ctx.clearRect(0,0,480,320);
+	game.frame++;
 	if(game.running){
-		game.frame++;
 		player.animate();
 		player.checkCollisions_coins();
 		player.checkCollisions_walls();
@@ -39,8 +42,10 @@ function game_loop(){
 		    wallGenerator();
 		}
 	}
-	renderCoins();
+	background.animate();
+	background.render();
 	renderWalls();
+	renderCoins();
 	player.render();
 	animateMenus();
 	animateTexts();
@@ -72,14 +77,17 @@ function renderWalls(){
 
 var player = {
 	x: 10,
-	y: 10,
 	w: 30,
 	h: 30,
+	y: 140,
 	dy: 0,
 	ddy: 0,
 	maxVel: 5,
 	points: 0,
 	totalPoints: 0,
+	r: 0,
+	g: 0,
+	b: 0,
 	animate: function(){
 	    if(game.awaitingInput){
 	        return;
@@ -101,9 +109,10 @@ var player = {
 		player.ddy *= 0.95;
 		player.dy *= 0.95;
 		
+		player.r = 120+ 20*Math.round(player.dy);
 	},
 	render: function(){
-		ctx.fillStyle = "black";
+		ctx.fillStyle = "rgb("+player.r+","+player.g+","+player.b+")";
 		ctx.fillRect(player.x,player.y,player.w,player.h);
 		//ctx.drawImage(player.spriteSheet,player.game.frameX,player.game.frameY,
 		//              player.x,player.y,player.game.frameW,player.game.frameH)
@@ -121,7 +130,7 @@ var player = {
 	},
 	checkCollisions_coins: function(){
 		for(var i=0; i<coins.length; i++){
-			if(player.willCollide(coins[i])){
+			if(player.willCollide(coins[i]) && coins[i].touchable){
 				coins[i].touched();
 			}
 		}
@@ -150,7 +159,7 @@ function animLoseScreen(){
 	};
 	text1 = new Text(495,80,"Game Over","48px Georgia",[255,255,255,1]);
 	text2 = new Text(580,140,"Score","32px Georgia",[255,255,255,1]);
-	text3 = new Text(605-(player.points.toString().length-1)*10,170,player.points,"38px Georgia",[255,255,255,1])
+	text3 = new Text(610-(player.points.toString().length-1)*10,170,player.points,"38px Georgia",[255,255,255,1])
 	setTimeout(function(){
 		text1.dx = -42;
 		text1.dy = 0;
@@ -183,8 +192,10 @@ function animLoseScreen(){
 			text1.dy = text2.dy = text3.dy = -42;
 			menu1.dx = -80;
 			del(button1);
+			del(text1);
+			del(text2);
+			del(text3);
 			button1 = null;
-			//buttons = [];
 			game.restart();
 		}
 	},1500)
@@ -194,15 +205,22 @@ var coin_vars = {
 	framesSinceLastCoin: 0,
 	y: 0,
 	theda: 0,
+	y_scaler: 110,
+	counter: 0,
 }
 
 function coinGenerator(){
 	coin_vars.framesSinceLastCoin ++;
 	if(coin_vars.framesSinceLastCoin == 10){
-	    coin_vars.y = 160+140*Math.sin(coin_vars.theda);
+		if(coin_vars.y_scaler >= 150){
+			coin_vars.y_scaler *= rand_d(0.9,1);
+		}
+		else{coin_vars.y_scaler *= rand_d(0.9,1.1);}
+	    coin_vars.y = 160+coin_vars.y_scaler*Math.sin(coin_vars.theda);
 		new Coin(500,coin_vars.y,1);
 		coin_vars.framesSinceLastCoin = 0;
 		coin_vars.theda += Math.PI/8;
+		coin_vars.counter ++;
 	}
 }
 function Coin(x,y,p){
@@ -212,6 +230,9 @@ function Coin(x,y,p){
 	this.points = p;
 	this.w = 15;
 	this.h = 15;
+
+	this.onScreen = true;
+	this.touchable = true;
 }
 Coin.prototype.touched = function(){
 	del(this);
@@ -222,10 +243,21 @@ Coin.prototype.touched = function(){
 Coin.prototype.animate = function(){
 	this.x -= 6;
 	if(this.x < -20){
-		//del(this);
+		this.onScreen = false;
+	}
+	for(var i=0; i<walls.length; i++){
+		var dx = walls[i].x - this.x;
+		var dy = walls[i].y - this.y;
+		var dist1 = dx*dx + dy*dy;
+
+		if(dist1 < 55*55){
+			this.onScreen = false;
+			this.touchable = false;
+		}
 	}
 }
 Coin.prototype.render = function(){
+	if(!this.onScreen){return}
 	ctx.fillStyle = "yellow";
 	ctx.fillRect(this.x,this.y,this.w,this.h)
 }
@@ -241,13 +273,19 @@ var wall_vars = {
 function wallGenerator(){
 	wall_vars.framesSinceLastWall ++;
 	if(wall_vars.framesSinceLastWall == 45){
-		wall_vars.y = 20+Math.floor(Math.random()*280)
+		wall_vars.y = player.y+rand_i(-120,120)
 		wall_vars.w = 20+Math.floor(Math.random()*40)
 		wall_vars.h = 20+Math.floor(Math.random()*40)
 
 		new Wall(500,wall_vars.y,wall_vars.w,wall_vars.h);
 		wall_vars.framesSinceLastWall = 0;
 	}
+}
+function rand_i(n1,n2){
+	return n1+Math.floor(Math.random()*(n2-n1));
+}
+function rand_d(n1,n2){
+	return n1+Math.random()*(n2-n1);
 }
 function Wall(x,y,w,h){
 	walls.push(this);
@@ -272,6 +310,60 @@ Wall.prototype.render = function(){
 
 	//ctx.drawImage(this.img,this.game.frameX,this.game.frameY,this.game.frameW,this.game.frameH,this.x,this.y,this.w,this.h)
 };
+var background_img = new Image();
+background_img.src = "desert_BG.png"
+var background = {
+	img: background_img,
+	x: 0,
+	y: 0,
+	w: 0,
+	h: 0,
+	animate: function(){
+		//background.x -= 3;
+		//if(background.x + background.w <= canvas.width){
+		//	background.x = 0;
+		//}
+		if(game.awaitingInput && !game.trigger1Fired){
+			s_frame = game.frame;
+			game.trigger1Fired = true;
+		}
+		if(game.awaitingInput && s_frame){
+			if(game.frame - s_frame > 180){
+				text4 = new Text(30,350,"Tap to begin","32px Georgia","0,0,0,1");
+				text4.dx = 0;
+				text4.dy = -10;
+				text4.animate = function(){
+					text4.dx *= 0.9;
+					text4.x += text4.dx;
+					text4.dy *= 0.9;
+					text4.y += text4.dy;
+					if(game.frame % 10 == 0){
+						text4.dy += 3*Math.sin(game.frame)
+					}
+				}
+				button2 = new Button(0,0,480,320);
+				button2.onTouch = function(){
+					game.awaitingInput = false;
+    				input.up = true;
+    				text4.dx = 60;
+					del(button2);
+					setTimeout(function(){
+						del(text4)
+					},750)
+					game.trigger1Fired = false;
+				}
+				s_frame = null;
+			}
+		}
+
+	},
+	render: function(){
+		//ctx.drawImage(background.img,-background.x,0,canvas.width,canvas.height,background.x,background.y,background.w,background.h)
+		ctx.drawImage(background.img,0,0)
+	},
+
+}
+
 
 function del(obj){
 	for(var i=0; i<coins.length; i++){
@@ -289,6 +381,12 @@ function del(obj){
 	for(var n=0; n<buttons.length; n++){
 		if(obj == buttons[n]){
 			buttons.splice(n,1);
+			return;
+		}
+	}
+	for(var t=0; t<texts.length; t++){
+		if(obj == texts[t]){
+			texts.splice(t,1);
 			return;
 		}
 	}
